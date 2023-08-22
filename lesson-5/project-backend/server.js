738bd7b3f22257158bb8ac9ba1f69868fb9878b7
unknown <bogdan.lyamzin.d@gmail.com>
2023-08-22 18:28:07 +0300

    move DB_HOST to config.js

--- lesson-5/project-backend/server.js
+++ lesson-5/project-backend/server.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const app = require('./app');
 
-const DB_HOST = "mongodb+srv://Bogdan:ILLnsIOJ1nOPYZfA@cluster0.oisyjmx.mongodb.net/my-movies?retryWrites=true&w=majority"
+const {DB_HOST} = require("./config")
 
 mongoose.connect(DB_HOST)
   .then(()=> {
